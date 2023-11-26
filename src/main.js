18034c6182f8cd1bc5254400cc1705ed814a4c44
SungminPark <byulparan@gmail.com>
2023-11-26 18:13:17 +0900

    add file resources/img.png

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
+import img from "./resources/img.png";
 
-let url = "/api/uc?id=1vWXyGXrZvO6W8vXiHLwVHbmdxEK6HIsp";
-
-fetch(url)
-  .then(data => data.blob())
-  .then(blob => URL.createObjectURL(blob))
-  .then(url =>  document.querySelector('#link').src = url)
+document.querySelector('#link').src = img;
 
 
 
